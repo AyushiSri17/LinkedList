e1b8e33e1c5195154736a3e3aa96b85bf9733229
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-14 22:44:34 +0530

    [Ayushi] Added Simple Linked List program

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -11,6 +11,14 @@ namespace LinkedList
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList Programs");
+
+            Console.WriteLine("Welcome To Linked List");
+            CustomLinkedList linkedlist = new CustomLinkedList();
+            linkedlist.AddLast(56);
+            linkedlist.AddLast(30);
+            linkedlist.AddLast(70);
+            linkedlist.Display();//56 30 70
+
             Console.ReadLine();
         }
     }
